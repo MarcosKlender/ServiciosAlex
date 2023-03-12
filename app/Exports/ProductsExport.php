0bb3a65c8d6f147bd6f817ae14ee2ee4205c9b28
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2023-03-12 01:03:35 -0500

    Report title added. Seeder updated.

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -6,13 +6,19 @@ use App\Models\Products;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class ProductsExport implements FromView, ShouldAutoSize
+class ProductsExport implements FromView, ShouldAutoSize, WithTitle
 {
     public function view(): View
     {
         return view('search.export', [
             'products' => Products::orderBy('id', 'asc')->get()
         ]);
+    }
+
+    public function title(): string
+    {
+        return 'stock';
     }
 }
